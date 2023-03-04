8ccbc0c28e7039f6cfb8c3621b72e880495db449
cColds <103373668+cColds@users.noreply.github.com>
2023-03-04 12:44:34 -0600

    Add merge sort

---
+++ mergeSort.js
@@ -0,0 +1,29 @@
+function merge(low, high) {
+	const mergeArray = [];
+	while (low[0] != null || high[0] != null) {
+		if (low[0] == null || high[0] == null) {
+			mergeArray.push(low[0] == null ? high.shift() : low.shift());
+		} else {
+			mergeArray.push(low[0] > high[0] ? high.shift() : low.shift());
+		}
+	}
+
+	return mergeArray;
+}
+
+function mergeSort(array) {
+	if (!array.length || !Array.isArray(array)) return "Unable to merge sort";
+	if (array.length === 1) return array;
+
+	const middle = Math.floor(array.length / 2);
+	const lowArray = mergeSort(array.slice(0, middle));
+	const highArray = mergeSort(array.slice(middle, array.length));
+
+	return merge(lowArray, highArray);
+}
+
+console.log(mergeSort("beans")); // Unable to merge sort
+console.log(mergeSort([])); // Unable to merge sort
+console.log(mergeSort([5])); // [5]
+console.log(mergeSort([3, 2, 4, 1, 7, 9, 8, 6, 5, 0])); // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(mergeSort([2, 2, 89, 15, 7, 4, 4])); // [2, 2, 4, 4, 7, 15, 89]
