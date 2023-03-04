636af56d775a85806e491c40033c59f45b19bb93
cColds <103373668+cColds@users.noreply.github.com>
2023-03-04 12:23:02 -0600

    Add recursive fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -12,8 +12,27 @@ function iterativeFibonacci(n) {
 	return sequence;
 }
 
+function recursiveFibonacci(n, sequence = [0, 1]) {
+	if (n <= 0 || !Number.isInteger(n))
+		return "Must be an integer of 0 or greater";
+	if (n === 1) return [0];
+	if (sequence.length === n) return sequence;
+
+	sequence.push(
+		sequence[sequence.length - 1] + sequence[sequence.length - 2]
+	);
+
+	return recursiveFibonacci(n, sequence);
+}
+
 console.log(iterativeFibonacci(0)); // "Must be an integer of 1 or greater"
 console.log(iterativeFibonacci(1)); //  [0]
 console.log(iterativeFibonacci(4)); // [0, 1, 1, 2]
 console.log(iterativeFibonacci(8)); // [0, 1, 1, 2, 3, 5, 8, 13]
 console.log(iterativeFibonacci(15)); // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
+
+console.log(recursiveFibonacci(0)); // "Must be an integer of 1 or greater"
+console.log(recursiveFibonacci(1)); //  [0]
+console.log(recursiveFibonacci(4)); // [0, 1, 1, 2]
+console.log(recursiveFibonacci(8)); // [0, 1, 1, 2, 3, 5, 8, 13]
+console.log(recursiveFibonacci(15)); // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
